4651d326524da3d3333fdfd669ba92e272f8a66b
Reshma-Chowdary <99269220+Reshma-Chowdary@users.noreply.github.com>
2022-02-08 21:22:15 +0530

    Add files via upload

---
+++ Comparision_Operators.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+print('a is less than b ->', a < b)
+print('b is less than a -> ', b < a)
+print('a is greater than b -> ', a > b)
+print('b is greater than a -> ', b > a)
+print('a is greater than or equal to b -> ', a >= b)
+print('b is greater than or equal to a -> ', b >= a)
+print('a is less than or equal to b -> ', a <= b)
+print('b is less than or equal to a -> ', b <= a)
+print('a is equal to b', a == b)
+print('a is not equal to b', a != b)
