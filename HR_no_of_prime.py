5fe8e765a48a4020d875f721399d7ad1f27a2d3e
Reshma-Chowdary <99269220+Reshma-Chowdary@users.noreply.github.com>
2022-02-20 09:44:59 +0530

    Update HR_no_of_prime.py

--- HR_no_of_prime.py
+++ HR_no_of_prime.py
@@ -2,7 +2,7 @@ n = int(input())
 for i in range(n):
     l, u = [int(x) for x in input().split()]
     count = 0
-    for i in range(l, u+1):
+    for i in range(l+1, u):
         k = 0
         for j in range(1, i+1):
             if(i % j == 0):
