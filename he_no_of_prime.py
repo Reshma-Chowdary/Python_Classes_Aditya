0f49c74113eac65c3f80c7af427e26e29ac39011
Reshma-Chowdary <99269220+Reshma-Chowdary@users.noreply.github.com>
2022-02-20 09:47:17 +0530

    Update he_no_of_prime.py

--- he_no_of_prime.py
+++ he_no_of_prime.py
@@ -1,8 +1,8 @@
 n = int(input())
-for l in range(n):
-    l = int(input())
+for i in range(n):
+    u = int(input())
     count = 0
-    for i in range(1, l+1):
+    for i in range(1, u+1):
         k = 0
         for j in range(1, i+1):
             if(i % j == 0):
