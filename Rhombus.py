c211027542310981698ae5bd0b425f9b6678168d
Reshma-Chowdary <99269220+Reshma-Chowdary@users.noreply.github.com>
2022-02-23 06:29:00 +0530

    Update Rhombus.py

--- Rhombus.py
+++ Rhombus.py
@@ -1,11 +1,7 @@
-n = int(input('Enter any odd number: '))
-k = n//2
-for i in range(1, n+1, 2):
-    print(' '*k + '*'*i, end='')
+n = int(input('Enter any number: '))
+for i in range(1, n+1):
+    print(' '*(n-i) + '* '*i, end='')
     print()
-    k -= 1
-k = 1
-for i in range(n-2, 0, -2):
-    print(' '*k + '*'*i, end='')
+for i in range(n-1, 0, -1):
+    print(' '*(n-i)+'* '*i, end='')
     print()
-    k += 1
